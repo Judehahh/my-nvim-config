9843b47001dc12a771427a0921824431f01d13ca
judehahh <liujh2818@outlook.com>
2024-02-19 16:46:45 +0800

    Added support for displaying ansi color.

--- init.lua
+++ init.lua
@@ -1,5 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-vim.api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
 
+-- to display ansi color
+vim.api.nvim_create_user_command("BaleiaColorize", function()
+  require("baleia").setup({}).once(vim.api.nvim_get_current_buf())
+end, { force = true })
---
+++ lua/plugins/ansiesc.lua
@@ -0,0 +1,11 @@
+return {
+  -- add AnsiEsc
+  {
+    "powerman/vim-plugin-AnsiEsc",
+    -- add your options that should be passed to the setup() function here
+    lazy = true,
+    cmd = {
+      "AnsiEsc",
+    },
+  },
+}
